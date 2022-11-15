2a087d8e1e5d77860c5b816e8f80e98c6a41c386
juananre <78058130+juananre@users.noreply.github.com>
2022-11-15 17:46:26 -0500

    cAA

--- taller_6_zapatos/Assets/scripts/BasicTouch.cs
+++ taller_6_zapatos/Assets/scripts/BasicTouch.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.SceneManagement;
-using TMPro;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class BasicTouch : MonoBehaviour
 {
     public Camera mainCamera;
+
+    [SerializeField] TMP_Text timer;
     
+    [SerializeField] int cambio_estado = 0;
+
+    [SerializeField] int min, seg;
+
+    private float restante;
+
     [SerializeField] GameObject insulto_1;
     [SerializeField] GameObject insulto_2;
     [SerializeField] GameObject insulto_3;
@@ -19,15 +28,21 @@ public class BasicTouch : MonoBehaviour
     [SerializeField] GameObject insulto_7;
     [SerializeField] GameObject insulto_8;
     [SerializeField] GameObject insulto_9;
+    [SerializeField] GameObject oscuridad_1;
+    [SerializeField] GameObject oscuridad_2;
+    [SerializeField] GameObject oscuridad_3;
+    [SerializeField] GameObject oscuridad_4;
+    [SerializeField] GameObject oscuridad_5;
 
     [SerializeField] int escena;
 
     [SerializeField] int puntaje;
     [SerializeField] int necesario;
 
-    [SerializeField] float timer;
+
 
     [SerializeField] Transform mapa;
+    
 
     bool verifi=false;
     private int velicidad=0;
@@ -37,6 +52,7 @@ public class BasicTouch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         //InstanciaCubos();
     }
 
@@ -60,20 +76,28 @@ public class BasicTouch : MonoBehaviour
         insultos();
         TouchPhaser();
 
-        if (Input.GetKeyDown(KeyCode.Space) ||puntaje== necesario || timer<=0)
+        
+        if (Input.GetKeyDown(KeyCode.Space) || puntaje== necesario || restante < 0)
         {
             SceneManager.LoadScene(escena);
         }
-
+       
     }
     private void FixedUpdate()
     {
         if (verifi == true)
         {
-            timer -= Time.deltaTime;
+            restante -= Time.deltaTime;
+            int tempmin = Mathf.FloorToInt(restante / 60);
+            int tempseg = Mathf.FloorToInt(restante % 60);
+            timer.text = string.Format("{00:00}:{01:00}", tempmin, tempseg);
+
         }
     }
-
+    public void Awake()
+    {
+        restante = (min * 60) + seg;
+    }
 
     void TouchPhaser() {
 
@@ -83,18 +107,19 @@ public class BasicTouch : MonoBehaviour
 
             if (t.phase == TouchPhase.Began) {
 
-                velicidad -=70;
+               
+                velicidad -=80;
                 mapa.position = new Vector3(quieto1, velicidad, quieto2);
                 puntaje++;
-                Debug.Log("Tap");
+                //Debug.Log("Tap");
             }
             if (t.phase == TouchPhase.Moved)
             {
-                Debug.Log("Moviendo");
+                //Debug.Log("Moviendo");
             }
             if (t.phase == TouchPhase.Ended)
             {
-                Debug.Log("Levantó");
+                //Debug.Log("Levantó");
             }
         }
 
@@ -105,43 +130,56 @@ public class BasicTouch : MonoBehaviour
         verifi = true;
         if (puntaje == 5)
         {
-
+            
             insulto_1.SetActive(true);
         }
         if (puntaje == 15)
         {
+            
             insulto_2.SetActive(true);
         }
         if (puntaje == 20)
         {
-            insulto_3.SetActive(true);
+            
+            insulto_2.SetActive(true);
         }
         if (puntaje == 30)
         {
+           
             insulto_4.SetActive(true);
         }
         if (puntaje == 45)
         {
+            oscuridad_1.SetActive(true);
             insulto_5.SetActive(true);
         }
         if (puntaje == 60)
         {
+            oscuridad_1.SetActive(false);
+            oscuridad_2.SetActive(true);
             insulto_6.SetActive(true);
         }
         if (puntaje == 70)
         {
+            oscuridad_2.SetActive(false);
+            oscuridad_3.SetActive(true);
             insulto_6.SetActive(true);
         }
         if (puntaje == 80)
         {
+            oscuridad_3.SetActive(false);
+            oscuridad_4.SetActive(true);
             insulto_7.SetActive(true);
         }
         if (puntaje == 88)
         {
+            oscuridad_4.SetActive(false);
+            oscuridad_5.SetActive(true);
             insulto_8.SetActive(true);
         }
         if (puntaje == 90)
         {
+            
             insulto_9.SetActive(true);
         }
     }
